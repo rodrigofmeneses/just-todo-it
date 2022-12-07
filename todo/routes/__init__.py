d27facf5270690a4c0d7019a5d76957d5149a9ec
rodrigofmeneses <rodrigo.menesesufc@gmail.com>
2022-12-07 15:19:02 -0300

    :bug: bug: Prefixo /auth removido

--- todo/routes/__init__.py
+++ todo/routes/__init__.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
-from .user import router as user_router
 from .auth import router as auth_router
+from .task import router as task_router
+from .user import router as user_router
 
 main_router = APIRouter()
 
+main_router.include_router(auth_router, tags=["auth"])
+main_router.include_router(task_router, prefix="/task", tags=["task"])
 main_router.include_router(user_router, prefix="/user", tags=["user"])
-main_router.include_router(auth_router, prefix="/auth", tags=["auth"])
